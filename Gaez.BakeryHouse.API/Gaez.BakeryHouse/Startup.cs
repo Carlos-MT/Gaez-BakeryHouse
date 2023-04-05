ef05e4ded29a5244bff7f435ba37f9e1fc7db550
Carlos Man <carlos@algo.com>
2023-04-05 14:56:02 -0600

    Se modifica la clase Startup y se agrega el servicio de Identity

--- Gaez.BakeryHouse.API/Gaez.BakeryHouse/Startup.cs
+++ Gaez.BakeryHouse.API/Gaez.BakeryHouse/Startup.cs
@@ -1,6 +1,45 @@
-﻿namespace Gaez.BakeryHouse
+﻿using Gaez.BakeryHouse.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gaez.BakeryHouse
 {
     public class Startup
     {
+        #region PROPERTIES
+        public IConfiguration Configuration { get; }
+        #endregion
+        #region CONSTRUCTOR
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        #endregion
+        #region METHODS
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoint => endpoint.MapControllers());
+        }
+        #endregion
     }
 }
