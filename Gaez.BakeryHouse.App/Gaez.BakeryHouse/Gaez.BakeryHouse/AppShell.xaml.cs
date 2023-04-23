c9cec74d03b12587e25b89cfc23a670061ffbf06
Carlos Manuel Torres <carlosmt1101@outlook.com>
2023-04-23 09:17:16 -0600

    Se limpia el proyecto

--- Gaez.BakeryHouse.App/Gaez.BakeryHouse/Gaez.BakeryHouse/AppShell.xaml.cs
+++ Gaez.BakeryHouse.App/Gaez.BakeryHouse/Gaez.BakeryHouse/AppShell.xaml.cs
@@ -1,23 +1,20 @@
-﻿using Gaez.BakeryHouse.Fonts;
-using Gaez.BakeryHouse.Models;
-using Gaez.BakeryHouse.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Gaez.BakeryHouse
 {
-    public partial class AppShell : Shell
-    {
-        public AppShell()
-        {
-            InitializeComponent();
-            Routing.RegisterRoute(nameof(ProductDetailPage), typeof(ProductDetailPage));
-            Routing.RegisterRoute(nameof(CategoryProductPage), typeof(CategoryProductPage));
-        }
-    }
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AppShell : Shell
+	{
+		public AppShell ()
+		{
+			InitializeComponent ();
+		}
+	}
 }
